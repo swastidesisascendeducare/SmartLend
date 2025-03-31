13ebacb5c2f98e92086123f577c56cc238af9373
Vidhi Arora <143219369+vidhi2919@users.noreply.github.com>
2025-03-31 22:16:12 +0530

    Update loanRoutes.js

--- Mongobackend/updated_backend/routes/loanRoutes.js
+++ Mongobackend/updated_backend/routes/loanRoutes.js
@@ -1,21 +1,3 @@
-// const express = require("express");
-
-// const { getLoanMatches, getBestCollaborativeFunding, fundLoan, requestLoan, getLoanDetails, getBorrowerMatches } = require("../controllers/loanController");
-
-// const Loan = require("../models/loanModel"); // Adjust path as needed
-// // const { getLoanMatches, getBestCollaborativeFunding, fundLoan, requestLoan, getBorrowerMatches } = require("../controllers/loanController");
-
-// const router = express.Router();
-// router.get("/:lenderId", getLoanDetails);
-
-// router.get("/match/:borrowerId", getLoanMatches);
-// router.get("/best-collaborative", getBestCollaborativeFunding);
-// router.post("/fund-loan", fundLoan);
-// router.post("/request-loan", requestLoan); // by vidhi on 26th March
-// router.get("/get-eligible-borrowers/:lenderId",getBorrowerMatches);
-
-// module.exports = router;
-
 const express = require("express");
 const {
   getLoanMatches,
@@ -26,7 +8,7 @@ const {
 
 const router = express.Router();
 
-// ✅ Loan routes
+// Loan routes
 router.get("/match/:borrowerId", getLoanMatches);
 router.get("/best-collaborative", getBestCollaborativeFunding);
 router.post("/fund-loan", fundLoan);
